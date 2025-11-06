b44b26fd5212ab8b5314bd9a77bea985a7b59a94
happylifedev <happyshr10.dekiru@gmail.com>
2025-11-06 12:19:02 +0900

    update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,14 +4,6 @@ import { Card } from '@/components/ui/card';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { ExternalLink, Github } from 'lucide-react';
 import defiPlatform from '../assets/projects/1.jpg';
-import nftMarketplace from '../assets/projects/2.jpg';
-import ecommerce from '../assets/projects/3.jpg';
-import solanaDex from '../assets/projects/4.jpg';
-import socialMedia from '../assets/projects/5.jpg';
-import contractAuditor from '../assets/projects/6.jpg';
-import daoPlatform from '../assets/projects/7.jpg';
-import projectManagement from '../assets/projects/8.jpg';
-import project9 from '../assets/projects/9.jpg';
 
 type ProjectCategory = 'all' | 'fullstack' | 'blockchain';
 
@@ -38,96 +30,7 @@ const projects: Project[] = [
     demo: 'https://dumpfun.vercel.app',
     fullDescription: 'Tools for Solana meme trading — track launchpads, view charts, and trade fast with Axiom or Gmgn.',
     image: defiPlatform,
-  },
-   {
-    id: 6,
-    title: 'FashionCorner',
-    description: 'FashionCorner - Next.js 14 Fullstack Ecommerce App',
-    category: 'fullstack',
-    tech: ['Python', 'Solidity', 'Machine Learning', 'React'],
-    github: 'https://github.com/happylifedev/FashionCorner',
-    demo: 'https://fashion-corner.vercel.app',
-    fullDescription: 'Fullstack Next.js  e-commerce app with MongoDB, Cloudinary, PayPay, and an admin dashboard for seamless store management.',
-    image: contractAuditor,
-  },
-  {
-    id: 2,
-    title: 'Pumpfun-smartcontract',
-    description: 'Fork of Pump.fun with core functionalities on Solana Devnet',
-    category: 'blockchain',
-    tech: ['rust', 'solana', 'smartcontract'],
-    github: 'https://github.com/happylifedev/ump.fun-Smart-Contract-Devnet-',
-    demo: 'https://bunt.fun/',
-    fullDescription: 'Fork of Pump.fun with core functionalities on Solana Devnet. Includes smart contract, token launch, swap, curve, and withdrawal. Frontend & backend available via GitHub. Contact: Telegram / WhatsApp / Twitter.',
-    image: nftMarketplace,
-  },
-  {
-    id: 3,
-    title: 'Solana-Casino-Game',
-    description: '',
-    category: 'blockchain',
-    tech: ['rust', 'typescript', 'solana', 'casino-game'],
-    github: 'https://github.com/happylifedev/casinogame',
-    demo: 'https://ggsol.io',
-    fullDescription: '',
-    image: ecommerce,
-  },
-  
-  {
-    id: 5,
-    title: 'Krycha-Store',
-    description: 'Fullstack Next.js e-commerce app',
-    category: 'fullstack',
-    tech: ['Next', 'typescript', 'javascript', 'scss'],
-    github: 'https://github.com/happylifedev/Krycha-Store',
-    demo: 'https://krycha-store.vercel.app',
-    fullDescription: 'Fullstack Next.js e-commerce app using Firebase, Stripe, and SSG for fast, secure, and SEO-optimized shopping.',
-    image: socialMedia,
-  },
-  {
-    id: 4,
-    title: ' Jupiter-Arbitrage Bot',
-    description: 'An advanced, non-custodial Solana arbitrage bot powered by Jupiter Aggregator V6.',
-    category: 'blockchain',
-    tech: ['Dex', 'typescript', 'Solana', 'React'],
-    github: 'https://github.com/happylifedev/abitragebot',
-    demo: 'https://solscan.io/tx/4SWQPhWbzAPyCgwk5g7frohM6FfeffgUg3occX2zzhCLpQuDxzmhEgb4dNiT914NfRT4JTYjq9n5aViAs3iwb9PU',
-    fullDescription: 'An advanced, non-custodial Solana arbitrage bot powered by Jupiter Aggregator V6. It scans DEXs in real time, builds multi-leg swap routes, and executes instant, MEV-protected trades for passive yield. Features include local routing, transaction racing, and QuickNode integration for ultra-fast, secure performance.',
-    image: solanaDex,
-  },
-  {
-    id: 7,
-    title: 'Muji Hotel',
-    description: 'A modern hotel booking platform built with Next.js, Sanity CMS, and Stripe for payments.',
-    category: 'fullstack',
-    tech: ['React.js', 'typescript', 'javascript', 'scss'],
-    github: 'https://github.com/happylifedev/StayModern',
-    demo: 'https://muji-hotel.vercel.app',
-    fullDescription: 'About Modern hotel booking platform built with Next.js, Sanity CMS, and Stripe for secure, seamless reservations.',
-    image: daoPlatform,
-  },
-  {
-    id: 8,
-    title: 'ecommerce-project',
-    description: 'Full-stack e-commerce solution with real-time admin dashboard',
-    category: 'fullstack',
-    tech: ['React.js', 'typescript', 'prisma', 'scss'],
-    github: 'https://github.com/happylifedev/Pogres?tab=readme-ov-file',
-    demo: 'https://ecommerce-project-admin-five.vercel.app',
-    fullDescription: 'Full-stack e-commerce solution with real-time admin dashboard, product management, and seamless store integration using Next.js, Prisma, and Stripe.',
-    image: projectManagement,
-  },
-  {
-    id: 9,
-    title: 'DApp-with-Ethers.js',
-    description: 'A simple DApp using Ethers.js to connect wallets, view USDC balances, and transfer tokens.',
-    category: 'blockchain',
-    tech: ['ethers.js', 'ethereum', 'Dapp', 'web3'],
-    github: 'https://github.com/happylifedev/DApp-with-Ethers.js',
-    demo: 'https://blog.logrocket.com/building-dapp-ethers-js/',
-    fullDescription: 'A simple DApp using Ethers.js to connect wallets, view USDC balances, and transfer tokens.',
-    image: project9 ,
-  },
+  }
  
 ];
 
